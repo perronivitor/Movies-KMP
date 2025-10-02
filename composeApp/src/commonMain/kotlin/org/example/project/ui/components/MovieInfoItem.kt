530664ce951d4393783ea51cdf308a1dbd204298
perronivitor <vitorope48@gmail.com>
2025-10-01 21:33:10 -0300

    Implementando o componente MovieGenreChip

---
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/components/MovieInfoItem.kt
@@ -0,0 +1,58 @@
+package org.example.project.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.Star
+import org.example.project.ui.theme.MoviesAppTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MovieInfoItem(
+    icon: ImageVector,
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier
+                .size(10.dp),
+            tint = Color.Gray
+        )
+
+        Text(
+            text = text,
+            modifier = Modifier
+                .padding(start = 2.dp),
+            color = Color.Gray,
+            style = MaterialTheme.typography.bodySmall,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MovieInfoItemPreview() {
+    MoviesAppTheme {
+        MovieInfoItem(
+            icon = FontAwesomeIcons.Solid.Star,
+            text = "7.5"
+        )
+    }
+}
